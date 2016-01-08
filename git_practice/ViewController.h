3efd8dbcffa60f1e359a55b64cbdf950e3889067
Kanybek Zhagusaev <zhagusaev@gmail.com>
2016-01-08 12:12:31 -0600

    first commit

---
+++ git_practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git_practice
+//
+//  Created by Kanybek Zhagusaev on 1/8/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git_practice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  git_practice
+//
+//  Created by Kanybek Zhagusaev on 1/8/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
